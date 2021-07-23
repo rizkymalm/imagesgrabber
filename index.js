91fd6ff5bf159913c30adc6b2b72eebbf065e513
Rizki <rizkymalm@gmail.com>
2021-07-23 23:27:59 +0700

    perbaikan

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express()
 
 const bodyParser = require("body-parser")
 const fileupload = require("express-fileupload")
+const fs = require("fs")
 require("dotenv").config()
 app.use(express.static("public"))
 app.use(fileupload())
-app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json())
 
 global.filesDir = function(){
@@ -16,6 +16,17 @@ global.filesDir = function(){
 
 app.get("", (req,res) => {
     res.send("test images grabber")
+})
+
+app.get("/:folder/:prdfilename", (req,res) => {
+    const path = filesDir()+"/"+req.params.folder+"/"+req.params.prdfilename
+        if(fs.existsSync(path)){
+            var image = fs.readFileSync(path)
+            res.send(image)
+        }else{
+            var image = fs.readFileSync(filesDir()+"/assets/errimage.png")
+            res.send(image)
+        }
 })
 
 app.listen(process.env.PORT, (err,res) => {
